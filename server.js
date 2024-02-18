59f9b06eee34eac9c43811ed4007a78119f48249
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2024-02-18 21:21:30 +0330

    bugfix

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const fs = require("fs");
+var cors = require('cors')
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || "0.0.0.0";
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
